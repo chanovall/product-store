d917d2f61459aa3595390eb4bddd609b656b1dae
chanovall <molchanova.vladislava@gmail.com>
2022-04-16 22:59:20 +0300

    add new

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './components/main/main.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button'
 
 
 
@@ -13,7 +16,10 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class MainModule { }
